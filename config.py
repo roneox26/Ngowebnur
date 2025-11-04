59b17395624ad8109eeeb7383a291ec4a71a648c
Rone <roneox16@gmail.com>
2025-11-04 11:56:10 +0600

    Add PostgreSQL support for Render

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
-SECRET_KEY = 'your_secret_key_here'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///loan.db'
+import os
+
+SECRET_KEY = os.environ.get('SECRET_KEY', 'your_secret_key_here')
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///loan.db')
+if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith('postgres://'):
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace('postgres://', 'postgresql://', 1)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 MAX_CONTENT_LENGTH = 16 * 1024 * 1024
